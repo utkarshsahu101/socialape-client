0806af01816b32ef047dd5ed509bc9146f08c6dd
utkarshsahu101 <utkarshsahu101@gmail.com>
2020-09-27 21:38:51 +0530

    scream dialog box is added

--- src/redux/actions/dataActions.js
+++ src/redux/actions/dataActions.js
@@ -8,6 +8,8 @@ import {
   SET_ERRORS,
   POST_SCREAM,
   LOADING_UI,
+  STOP_LOADING_UI,
+  SET_SCREAM,
 } from "../types";
 import axios from "axios";
 
@@ -28,6 +30,20 @@ export const getScreams = () => (dispatch) => {
         payload: [],
       });
     });
+};
+
+export const getScream = (screamId) => (dispatch) => {
+  dispatch({ type: LOADING_UI });
+  axios
+    .get(`/scream/${screamId}`)
+    .then((res) => {
+      dispatch({
+        type: SET_SCREAM,
+        payload: res.data,
+      });
+      dispatch({ type: STOP_LOADING_UI });
+    })
+    .catch((err) => console.log(err));
 };
 
 // Post a scream
@@ -81,4 +97,8 @@ export const deleteScream = (screamId) => (dispatch) => {
       dispatch({ type: DELETE_SCREAM, payload: screamId });
     })
     .catch((err) => console.log(err));
+};
+
+export const clearErrors = () => (dispatch) => {
+  dispatch({ type: CLEAR_ERRORS });
 };
--- src/redux/reducers/uiReducer.js
+++ src/redux/reducers/uiReducer.js
@@ -1,5 +1,10 @@
 import { act } from "react-dom/test-utils";
-import { SET_ERRORS, CLEAR_ERRORS, LOADING_UI } from "../types";
+import {
+  SET_ERRORS,
+  CLEAR_ERRORS,
+  LOADING_UI,
+  STOP_LOADING_UI,
+} from "../types";
 
 const initialState = {
   loading: false,
@@ -24,6 +29,11 @@ export default function (state = initialState, action) {
       return {
         ...state,
         loading: true,
+      };
+    case STOP_LOADING_UI:
+      return {
+        ...state,
+        loading: false,
       };
     default:
       return state;
